71eb0b31de87cb88c751d179e47898f44009c3bb
Harry Sullivan <aditya.michigan@gmail.com>
2019-09-08 15:08:33 -0400

    cache busting

---
+++ utils/cache.js
@@ -0,0 +1,11 @@
+const cheerio = require('cheerio')
+const fs = require('fs');
+
+var data = fs.readFileSync('dist/index.html', 'utf-8');
+const $ = cheerio.load(data)
+
+const now = new Date();
+
+$("meta[http-equiv=last-modified]").attr("content", now.toUTCString());
+
+fs.writeFileSync("dist/index.html", $.html());
