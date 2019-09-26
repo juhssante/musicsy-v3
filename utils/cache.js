2905db57289af55143d3e03bb7017da98bf00b29
Harry Sullivan <aditya.michigan@gmail.com>
2019-09-25 21:11:05 -0400

    ashwat has lady issues

--- utils/cache.js
+++ utils/cache.js
@@ -1,15 +1,3 @@
-// const cheerio = require('cheerio')
-// const fs = require('fs');
-
-// var data = fs.readFileSync('dist/index.html', 'utf-8');
-// const $ = cheerio.load(data)
-
-// const now = new Date();
-
-// $("meta[http-equiv=last-modified]").attr("content", now.toUTCString());
-
-// fs.writeFileSync("dist/index.html", $.html());
-
 const fs = require('fs');
 
 fs.writeFileSync('.etag', Math.random());
